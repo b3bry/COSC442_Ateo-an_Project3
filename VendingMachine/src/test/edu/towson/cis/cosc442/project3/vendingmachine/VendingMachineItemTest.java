a69933092f6ec25f82a1d18287b0ef36f0873c11
b3bry <bateoa1@students.towson.edu>
2017-02-28 14:53:17 -0500

    Initial Commit

---
+++ VendingMachine/src/test/edu/towson/cis/cosc442/project3/vendingmachine/VendingMachineItemTest.java
@@ -0,0 +1,60 @@
+package test.edu.towson.cis.cosc442.project3.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import edu.towson.cis.cosc442.project3.vendingmachine.VendingMachineException;
+import edu.towson.cis.cosc442.project3.vendingmachine.VendingMachineItem;
+
+public class VendingMachineItemTest {
+	VendingMachineItem vi;
+	@Before
+	public void setUp() throws Exception {
+		vi = new VendingMachineItem("chips",9.99);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	/**
+	 * test constructing a vending machine item with valid arguments does not fail
+	 */
+	@Test
+	public void testVendingMachineItem() {
+		try{
+			@SuppressWarnings("unused")
+			VendingMachineItem localvi = new VendingMachineItem("coke",1.99);
+		}catch (Exception e){
+			fail(e.getMessage());
+		}
+	}
+	
+	/**
+	 * Test precondition, price >=0
+	 */
+	@Test(expected=VendingMachineException.class)
+	public void testVendingMachineItem_negPrice(){
+		new VendingMachineItem("steak",-0.01);
+	}
+	
+	/**
+	 * Tests getName returns the right name
+	 */
+	@Test
+	public void testGetName() {
+		assertEquals("chips",vi.getName());
+	}
+
+	/**
+	 * Test getPrice returns the right price
+	 */
+	@Test
+	public void testGetPrice() {
+		assertEquals(9.99,vi.getPrice(),0.00);
+	}
+
+}
